424750d1b347fac3b6709f2065715f8f110e695c
Danniesantos <danielardesantos@gmail.com>
2025-09-02 15:28:56 -0300

    commit inicial

---
+++ src/test/java/com/daniela/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniela.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
